02beacccdc84ee21a4ffe0d3a3388c90ccda4c36
G.Naresh <70382020+Naresh79814@users.noreply.github.com>
2020-10-16 08:18:27 +0530

    Intitial

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+    var x int
+    x=3  
+    fmt.Println("x:", x) 
+    var y int=20
+    fmt.Println("y:", y)
+    var z=50
+    fmt.Println("z:", z)
+    var i, j = 100,"hello"
+    fmt.Println("i and j:", i,j)
+}
